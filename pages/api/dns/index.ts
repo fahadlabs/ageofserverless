240b25aa25c81a3e36b8810093336d951915426e
Fahad Hossain <fahad@avalonx.io>
2021-10-30 21:27:18 +0600

    unlink files

--- pages/api/dns/index.ts
+++
@@ -1,8 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { withAuth } from '../../../src/middlewares';
-
-async function Test(_: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ message: 'API message' });
-}
-
-export default withAuth(Test);
