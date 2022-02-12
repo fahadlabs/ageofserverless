0e520105a1868c58e3ad820f712bb3d0dc7336bb
Fahad Hossain <fahadh.dev@gmail.com>
2022-02-12 22:56:51 +0600

    Add ENVs

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
  * @type {import('next').NextConfig}
  */
 
+const { FIREBASE_API_KEY, FIREBASE_AUTH_DOMAIN, FIREBASE_DATABASE_URL, FIREBASE_PROJECT_ID, FIREBASE_STORAGE_BUCKET, FIREBASE_MESSAGING_SENDER_ID, FIREBASE_APP_ID, FIREBASE_MEASUREMENT_ID } =
+  process.env;
+
 module.exports = {
   bodyParser: {
     sizeLimit: '1mb',
