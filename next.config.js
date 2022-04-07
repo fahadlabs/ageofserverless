d94283f5c4eb1cbf0c2d8730ab2d5a11323dcded
Fahad Hossain <fahadlabs@outlook.com>
2022-04-08 04:36:30 +0600

    Get things ready for deployment #26

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
-/**
- * @type {import('next').NextConfig}
- */
-
 const { FIREBASE_API_KEY, FIREBASE_AUTH_DOMAIN, FIREBASE_DATABASE_URL, FIREBASE_PROJECT_ID, FIREBASE_STORAGE_BUCKET, FIREBASE_MESSAGING_SENDER_ID, FIREBASE_APP_ID, FIREBASE_MEASUREMENT_ID } =
   process.env;
 
+/**
+ * @type {import('next').NextConfig}
+ */
 module.exports = {
   bodyParser: {
     sizeLimit: '1mb',
@@ -18,6 +17,9 @@ module.exports = {
     FIREBASE_MESSAGING_SENDER_ID,
     FIREBASE_APP_ID,
     FIREBASE_MEASUREMENT_ID,
+  },
+  experimental: {
+    outputStandalone: true,
   },
   reactStrictMode: true,
   poweredByHeader: false,
