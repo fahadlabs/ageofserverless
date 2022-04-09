13a56a92ceb1b77b039c72b4d4319a773c5dcb2a
Fahad Hossain <fahadlabs@outlook.com>
2022-04-09 15:27:07 +0600

    edit firebase project test

--- src/tests/firebase-config.spec.ts
+++ src/tests/firebase-config.spec.ts
@@ -3,7 +3,7 @@ import { firebaseConfig } from '../config/firebase';
 const firebaseProjectIds = ['axt-stage', 'aoslcom'];
 
 describe('Use Firebase project', () => {
-  test(`should use ${firebaseProjectIds.join(' or ')}, current: ${firebaseConfig.projectId}`, () => {
+  test(`should use appropriate firebase project id.`, () => {
     expect(firebaseProjectIds.includes(firebaseConfig.projectId)).toBe(true);
   });
 });
